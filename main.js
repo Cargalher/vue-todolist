bc85dbe8cdfa6e53166cf4d8df5e2375986279e8
Cargalher <cargalher@gmail.com>
2021-05-19 15:06:31 +0200

    Function addTask pushed newtask

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const app = new Vue({
     el: '#root',
    
     data:{
+        newTask: '',
         myTasks: ['Learn HTML', 'Learn CSS', 'Learn JS', 'Learn Vue'],
     },
     methods:{
-
+        addTask(){
+            this.myTasks.push(this.newTask)
+        }
     }
 })
