90dce173df4c5c3ee01931f34bbc547808e0ea09
Cargalher <cargalher@gmail.com>
2021-05-20 09:37:40 +0200

    comments

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const app = new Vue({
         title2: 'Completed',
         newTask: '',
         myTasks: ['Learn HTML', 'Learn CSS', 'Learn JS', 'Learn Vue'],
-        myCompletedTasks: ['']
+        myCompletedTasks: ''
     },
     methods:{
         addTask(){
@@ -19,7 +19,8 @@ const app = new Vue({
             this.myTasks.splice(index,1)
         },
         addFinishedTask(index){
-            this.addTask.push(this.myCompletedTasks)
+            this.myTasks.splice(index,1)
+            this.myTasks.push(this.myCompletedTasks)
             this.myCompletedTasks = ''
         }
     }
